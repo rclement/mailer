560d93960382ea26236042d5601288f86de799ca
Romain Clement <r.clement88@gmail.com>
2019-01-09 21:04:20 +0100

    Bump version to 0.2.0

--- mailer/__about__.py
+++ mailer/__about__.py
@@ -1,5 +1,5 @@
 __title__ = 'mailer'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __description__ = 'Simple mailer micro-service for static websites'
 __author__ = 'Romain Clement'
 __author_email__ = 'r.clement88@gmail.com'
