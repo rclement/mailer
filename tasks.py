8075826a35c158d06bf7f403a1dc2fd9495ecf6d
Romain Clement <r.clement88@gmail.com>
2019-04-28 19:07:06 +0200

    Use black for linting and reformating

--- tasks.py
+++ tasks.py
@@ -7,31 +7,25 @@ tests_path = "tests"
 
 @task
 def test(ctx):
-    cmd = 'py.test -v --cov={app_path} --cov-report term-missing tests'.format(
-        app_path=app_path
-    )
+    cmd = f"py.test -v --cov={app_path} --cov-report term-missing tests"
     ctx.run(cmd, pty=True)
 
 
 @task
 def safety(ctx):
-    cmd = 'safety check'
+    cmd = "safety check"
     ctx.run(cmd)
 
 
 @task
 def lint(ctx):
-    cmd = 'flake8 --ignore E501 {app_path} {tests_path}'.format(
-        app_path=app_path, tests_path=tests_path
-    )
+    cmd = f"black --check {app_path} {tests_path}"
     ctx.run(cmd)
 
 
 @task
 def reformat(ctx):
-    cmd = 'black {app_path} {tests_path}'.format(
-        app_path=app_path, tests_path=tests_path
-    )
+    cmd = f"black {app_path} {tests_path}"
     ctx.run(cmd)
 
 
@@ -41,24 +35,13 @@ def qa(ctx):
 
 
 @task
-def docker_deploy(ctx, username=None, password=None, repository=None, tag='latest'):
+def docker_deploy(ctx, username=None, password=None, repository=None, tag="latest"):
     if username and password and repository and tag:
-        cmd = 'echo {password} | docker login -u {username} --password-stdin'.format(
-            username=username, password=password
-        )
+        cmd = f"echo {password} | docker login -u {username} --password-stdin"
         ctx.run(cmd)
 
-        cmd = 'docker build -t {username}/{repository} .'.format(
-            username=username, repository=repository
-        )
+        cmd = f"docker build -t {username}/{repository}:{tag} ."
         ctx.run(cmd)
 
-        cmd = 'docker tag {username}/{repository} {username}/{repository}:{tag}'.format(
-            username=username, repository=repository, tag=tag
-        )
-        ctx.run(cmd)
-
-        cmd = 'docker push {username}/{repository}:{tag}'.format(
-            username=username, repository=repository, tag=tag
-        )
+        cmd = f"docker push {username}/{repository}:{tag}"
         ctx.run(cmd)
