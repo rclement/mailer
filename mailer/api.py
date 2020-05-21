c060dd44bb8b255da38152d5dfcea3b21cf3122c
Romain Clement <contact@romain-clement.net>
2020-05-21 18:39:30 +0200

    Add OpenAPI documentation to ApiInfoSchema

--- mailer/api.py
+++ mailer/api.py
@@ -12,9 +12,9 @@ router = APIRouter()
 
 
 class ApiInfoSchema(BaseModel):
-    name: str
-    version: str
-    api_version: str
+    name: str = Field(..., title="Name", description="Name of the app")
+    version: str = Field(..., title="Version", description="Version of the app")
+    api_version: str = Field(..., title="API Version", description="Version of the API")
 
 
 class MailSchema(BaseModel):
